727897e7e6242e3239574f2de30f009c50070f0d
Nikith Kalu <nikithkalu@gmail.com>
2022-04-04 12:58:51 +1000

    First version of colour changer
    Changes red, green and blue to black

--- prac2.m
+++ prac2.m
@@ -1,20 +1,27 @@
-clc, clear
-
-rgb = imread('coloredChips.png');
-[le,w,~]=size(rgb);
-
-Xo=rgb;
-
-for r=1:le
-    for k=1:w
-        if rgb(r,k,1)>127 && rgb(r,k,2)<127 && rgb(r,k,3)<127
-            Xo(r,k,1)=0;
-            Xo(r,k,2)=0;
-            Xo(r,k,3)=0;
-        end
-    end
-
+clc
+%Prac 2, Change a colour to black program
+X = imread('coloredChips.png');
+%Creates a user menu. With inputs of a,b,c
+disp('colour change program')
+disp('Select colour to change')
+disp('a. Red')
+disp('b. Green')
+disp('c. Blue')
+x = input('enter selection:', 's'); %asks user for value
+%
+%uses the switch case statement to call rgb function
+switch x
+   case 'a' %changes colour to red
+       newX = rgb(X, 'r', 'k');
+   case 'b' %changes colour to green
+       newX = rgb(X, 'g', 'k');
+   case 'c' %changes colour to blue
+       newX = rgb(X, 'b', 'k');
+   otherwise %if incorrect option is chosen displays error message
+       error('Incorrect menu option selected')
 end
+%displays the original image and the changed colour image.
+figure(1), imshow(X)
+figure(2), imshow(newX)
 
-figure(1)
-imshow()
+
--- rgb.m
+++ rgb.m
@@ -1,6 +1,6 @@
 function imout = rgb(imin, colourchangefrom, colourchangeto)
 % image is imin
-[len, wid, ~] =  size(imin);
+[len, wid, ~] = size(imin);
 imout = imin; 
 
 % Determine 
@@ -30,4 +30,26 @@ for k = 1:len
 
         switch colourchangefrom
             case 'r'
-                if (imin(k,l,1) < 127) && (imin(k,l,2) > 127)
+                if (imin(k,l,1) > 127) && (imin(k,l,2) < 127) && (imin(k,l,3) < 127)
+                    imout(k,l,1)=red;
+                    imout(k,l,2)=green;
+                    imout(k,l,3)=blue;
+                end
+            case 'g'
+                if (imin(k,l,1) < 127) && (imin(k,l,2) > 127) && (imin(k,l,3) < 127)
+                    imout(k,l,1)=red;
+                    imout(k,l,2)=green;
+                    imout(k,l,3)=blue;
+                end
+            case 'b'
+                if (imin(k,l,1) < 127) && (imin(k,l,2) < 127) && (imin(k,l,3) > 127)
+                    imout(k,l,1)=red;
+                    imout(k,l,2)=green;
+                    imout(k,l,3)=blue;
+                end
+            otherwise
+                error('Incorrect Parameters')
+        end     
+    end
+end
+
